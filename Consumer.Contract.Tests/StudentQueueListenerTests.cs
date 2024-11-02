e259ff5632967ff785fa5a005df962211bc2486d
Bohdan Tron <bohdan.tron@wineshipping.com>
2024-11-02 20:49:28 +0200

    consumer messaging queue

---
+++ Consumer.Contract.Tests/StudentQueueListenerTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using PactNet;
+using PactNet.Matchers;
+using PactNet.Output.Xunit;
+using Xunit.Abstractions;
+
+namespace Consumer.Contract.Tests
+{
+    public class StudentQueueListenerTests
+    {
+        private readonly IMessagePactBuilderV4 _messagePactBuilder;
+
+        public StudentQueueListenerTests(ITestOutputHelper output)
+        {
+            _messagePactBuilder = Pact.V4("Student Queue Listener", "Student Queue Publisher", new PactConfig
+            {
+                PactDir = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName + "/pacts",
+                DefaultJsonSettings = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                },
+                Outputters = [new XunitOutput(output)],
+                LogLevel = PactLogLevel.Debug
+            }).WithMessageInteractions();
+        }
+
+        [Fact]
+        public void ReceiveStudentCreatedEvent()
+        {
+            _messagePactBuilder
+                .ExpectsToReceive("a create student")
+                .Given("a student is pushed to the queue")
+                    .WithJsonContent(new
+                    {
+                        StudentId = Match.Integer(10),
+                        FirstName = "James",
+                        LastName = "Hetfield",
+                        Gender = "male",
+                        Address = Match.Null(),
+                        StandardId = Match.Integer(1),
+                    })
+                .Verify<StudentCreatedEvent>(message =>
+                {
+                    message.Should().BeEquivalentTo(new StudentCreatedEvent
+                    {
+                        StudentId = 10,
+                        FirstName = "James",
+                        LastName = "Hetfield",
+                        //Address = "1234, 56th Street, San Francisco, USA",
+                        Gender = "male",
+                        StandardId = 1
+                    });
+                });
+        }
+    }
+}
--- Consumer/StudentCreatedEvent.cs
+++ Consumer/StudentCreatedEvent.cs
@@ -5,8 +5,9 @@
         public required int StudentId { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
+        public required string Gender { get; set; }
         public int StandardId { get; set; }
         public string? Address { get; set; }
-        public string? Gender { get; set; }
+        
     }
 }
